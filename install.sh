cbaeb114164c667486a365781a05dce024a4fd8b
Eno Compton 4 <eno4@ecom.com>
2013-11-03 14:48:03 -0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [[ -e $HOME/.vimrc || -e $HOME/.vim ]]; then
+  echo "Error: ~/.vimrc or ~/.vim already exists."
+else
+  echo === Checking out vundle
+  git submodule update --init
+  cd -
+
+  echo === Linking vim directories
+  ln -s $PWD/vimrc $HOME/.vimrc
+  ln -s $PWD $HOME/.vim
+
+  echo === Vundling
+  vim +BundleInstall +qall
+
+  echo === Done
+fi
