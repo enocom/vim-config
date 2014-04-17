648bdc98e51b474c15aa4efe75dc239b657337ee
Eno Compton 4 <eno4@ecom.com>
2014-04-16 23:21:26 -0700

    Exit with non-zero code when vim dir exists

--- install.sh
+++ install.sh
@@ -6,16 +6,17 @@ echo "=== Installing Vim config files"
 if [[ -e $HOME/.vimrc || -e $HOME/.vim ]]; then
   echo "Error: ~/.vimrc or ~/.vim already exists."
   echo "Move them and then try again."
-else
-  echo === Checking out vundle
-  git submodule update --init
-
-  echo === Linking vim directories
-  ln -s $PWD/vimrc $HOME/.vimrc
-  ln -s $PWD $HOME/.vim
-
-  echo === Vundling
-  vim +BundleInstall +qall
-
-  echo === Done
+  exit 1
 fi
+
+echo === Checking out vundle
+git submodule update --init
+
+echo === Linking vim directories
+ln -s $PWD/vimrc $HOME/.vimrc
+ln -s $PWD $HOME/.vim
+
+echo === Vundling
+vim +BundleInstall +qall
+
+echo === Done
