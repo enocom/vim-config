66b9be38b5da066809215ce5778a24fce86ac3a7
Eno Compton 4 <eno4@ecom.com>
2016-12-18 14:09:18 -0700

    Update submodules as part of install

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ set -e
 
 echo "=== Installing Vim config files"
 
+git submodule update --init
+
 if [ -e $HOME/.vimrc ] || [ -e $HOME/.vim ]; then
   echo "Error: ~/.vimrc or ~/.vim already exists."
   echo "Move them and then try again."
